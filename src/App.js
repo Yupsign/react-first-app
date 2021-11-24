b7ffd58e738ac365826410a3a83eae13f70911b2
Yupsign <miguthree9927@gmail.com>
2021-11-24 20:57:30 +0900

    0.0.2ver

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Day from './component/Day';
+import DayList from './component/DayList';
 import Header from './component/Header';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <DayList />
+      <Day />
     </div>
   );
 }
